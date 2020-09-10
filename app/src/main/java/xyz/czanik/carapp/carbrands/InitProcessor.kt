ccb7202988e57f0bb498154fd815a6cdeab7e5cf
Kamil Czanik <kamil@czanik.xyz>
2020-09-10 22:55:35 +0200

    Implement InitProcessor.kt

---
+++ app/src/main/java/xyz/czanik/carapp/carbrands/InitProcessor.kt
@@ -0,0 +1,25 @@
+package xyz.czanik.carapp.carbrands
+
+import io.reactivex.rxjava3.core.Observable
+import xyz.czanik.carapp.Processor
+import xyz.czanik.carapp.Repository
+import xyz.czanik.carapp.TaskResult
+import xyz.czanik.carapp.carbrands.CarBrandsContract.Event
+import xyz.czanik.carapp.carbrands.CarBrandsContract.Result
+
+class InitProcessor(
+    private val carBrandsRepository: Repository<Unit, List<CarBrand>>
+) : Processor<Event.Init, TaskResult<Result>> {
+
+    override fun process(events: Observable<Event.Init>): Observable<TaskResult<Result>> = events.switchMap { fetchCarBrands() }
+
+    private fun fetchCarBrands(): Observable<TaskResult<Result>> = carBrandsRepository.get(Unit)
+            .toObservable()
+            .map(::success)
+            .onErrorReturn(::failure)
+            .startWithItem(TaskResult.InProgress())
+
+    private fun success(brands: List<CarBrand>?): TaskResult<Result> = TaskResult.Success(Result.CarBrands(brands))
+
+    private fun failure(cause: Throwable): TaskResult.Failure<Result> = TaskResult.Failure(cause)
+}
