a6c5255b41d6060db6c1ba01d2d2d1c58e689efc
Kamil Czanik <kamil@czanik.xyz>
2020-09-11 22:52:16 +0200

    Implement CarBrandClickedProcessor.kt

---
+++ app/src/main/java/xyz/czanik/carapp/carbrands/CarBrandClickedProcessor.kt
@@ -0,0 +1,14 @@
+package xyz.czanik.carapp.carbrands
+
+import io.reactivex.rxjava3.core.Observable
+import xyz.czanik.carapp.Processor
+import xyz.czanik.carapp.TaskResult
+import xyz.czanik.carapp.carbrands.CarBrandsContract.Event.CarBrandClicked
+import xyz.czanik.carapp.carbrands.CarBrandsContract.Result.CarBrandSelected
+
+class CarBrandClickedProcessor : Processor<CarBrandClicked, TaskResult<CarBrandSelected>> {
+
+    override fun process(events: Observable<CarBrandClicked>): Observable<TaskResult<CarBrandSelected>> = events.map(::selectCarBrand)
+
+    private fun selectCarBrand(event: CarBrandClicked) = TaskResult.Success(CarBrandSelected(event.index))
+}
---
+++ app/src/test/java/xyz/czanik/carapp/carbrands/CarBrandClickedProcessorTest.kt
@@ -0,0 +1,24 @@
+package xyz.czanik.carapp.carbrands
+
+import io.reactivex.rxjava3.subjects.PublishSubject
+import org.junit.jupiter.api.Test
+import xyz.czanik.carapp.TaskResult
+import xyz.czanik.carapp.carbrands.CarBrandsContract.*
+
+internal class CarBrandClickedProcessorTest {
+
+    private val SUT = CarBrandClickedProcessor()
+    private val events = PublishSubject.create<Event.CarBrandClicked>()
+    private val resultTester = SUT.process(events).test()
+
+    @Test
+    fun `test no results without event`() {
+        resultTester.assertNoValues()
+    }
+
+    @Test
+    fun `test SUT returns success result with clicked index`() {
+        events.onNext(Event.CarBrandClicked(1))
+        resultTester.assertValue(TaskResult.Success(Result.CarBrandSelected(1)))
+    }
+}
